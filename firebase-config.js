2367db7c8c79c6a9e7524a0f97e8360a45637530
nathangbs <nathan.g@grpotencial.com.br>
2025-05-16 09:50:21 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCte0cRbPuoZioxTgDSg6eJ3c1TC3SEvPs",
   authDomain: "checklist-frota-a4b3b.firebaseapp.com",
   projectId: "checklist-frota-a4b3b",
-  storageBucket: "checklist-frota-a4b3b.appspot.com", // corrigido aqui
+  storageBucket: "checklist-frota-a4b3b.appspot.com",
   messagingSenderId: "428306058067",
   appId: "1:428306058067:web:1f610791e68c5d75ecefa6"
 };
